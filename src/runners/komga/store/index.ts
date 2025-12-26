90b2f1b1e34c8e67b7cec7a96d45fb540ee12095
Emixam <crmaxime@gmail.com>
2025-12-26 14:52:25 +0100

    Update packages

--- src/runners/komga/store/index.ts
+++
@@ -1,6 +0,0 @@
-export const KomgaStore = {
-  host: () => ObjectStore.string("host"),
-  credentials: () => SecureStore.string("credentials"),
-  authenticated: () => ObjectStore.get("authenticated"),
-  openSeriesAsTitle: () => ObjectStore.boolean("openAsTitle"),
-};
